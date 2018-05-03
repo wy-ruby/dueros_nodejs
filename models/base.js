0780c22af919061025aeb64183c2b88845cfcd78
zhangxuesong <453025053@qq.com>
2018-05-03 13:28:39 +0800

    F 修复models/base.js mongdb变量生命错误

--- models/base.js
+++ models/base.js
@@ -2,7 +2,7 @@
 
 const MongoClient = require('mongodb').MongoClient;
 const MONGO_URL = 'mongodb://user:polyhome@60.205.151.71:57017/polydb';
-const mogo_db;
+let mogo_db;
 
 MongoClient.connect(MONGO_URL, function(err, db){
     if (err){
