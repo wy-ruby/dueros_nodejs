4df7926e996770ff85e3d8cbbc8ff227158acac1
王允 <906971957@qq.com>
2018-05-07 10:59:28 +0800

    A 添加了pm2的自动部署配置文件

---
+++ ecosystem.config.js
@@ -0,0 +1,52 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps : [
+
+    // First application
+    {
+      name      : 'duer-node',
+      script    : 'app.js',
+      env: {
+        COMMON_VARIABLE: 'true'
+      },
+      env_production : {
+        NODE_ENV: 'production'
+      }
+    },
+
+    // Second application
+    // {
+    //   name      : 'WEB',
+    //   script    : 'web.js'
+    // }
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy : {
+    production : {
+      user : 'live',
+      host : '123.57.139.200',
+      ref  : 'origin/master',
+      repo : 'git@git.ourjujia.com:zhangxuesong/duer-node.git',
+      path : '/home/live/dueros-wangyun',
+      'post-deploy' : 'cnpm install && pm2 reload ecosystem.config.js --env production --watch'
+    },
+    dev : {
+      user : 'node',
+      host : '212.83.163.1',
+      ref  : 'origin/master',
+      repo : 'git@github.com:repo.git',
+      path : '/var/www/development',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env dev',
+      env  : {
+        NODE_ENV: 'dev'
+      }
+    }
+  }
+};
